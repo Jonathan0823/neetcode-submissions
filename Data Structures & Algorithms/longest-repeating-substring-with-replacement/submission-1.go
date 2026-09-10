c3d196a3df722a3e64f0544eca5fbf7ccd68bf44
Guinn <146400304+Jonathan0823@users.noreply.github.com>
2026-09-10 07:00:09 +0700

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.go
@@ -0,0 +1,20 @@
+func characterReplacement(s string, k int) int {
+	charFreq := make(map[byte]int)
+	l, maxF, res := 0, 0, 0
+
+	for r:= 0; r < len(s); r++ { 
+		charFreq[s[r]]++
+
+		maxF = max(maxF, charFreq[s[r]])
+
+		if ((r-l + 1) - maxF > k) { 
+			charFreq[s[l]]--
+			l++
+		}
+
+		res = max(res, r-l + 1)
+	}
+
+	return res
+
+}
