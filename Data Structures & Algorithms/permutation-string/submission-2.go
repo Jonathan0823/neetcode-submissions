e17fb72b3c22f250e77d1d2e4671a5cfbae69974
Guinn <146400304+Jonathan0823@users.noreply.github.com>
2026-09-20 07:37:20 +0700

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.go
@@ -0,0 +1,29 @@
+func checkInclusion(s1 string, s2 string) bool {
+	var s1ArrayFreq, windowFreq [26]int
+	if len(s2) < len(s1) { 
+		return false
+	}
+	for i := 0; i< len(s1);i++ { 
+		s1ArrayFreq[s1[i] - 'a']++
+		windowFreq[s2[i] - 'a']++
+	}
+
+	if s1ArrayFreq == windowFreq { 
+			return true
+		}
+
+
+	for r:= len(s1); r < len(s2); r++ { 
+		windowFreq[s2[r]-'a']++
+		windowFreq[s2[r-len(s1)] - 'a']--	
+		
+
+		if s1ArrayFreq == windowFreq { 
+			return true
+		}
+
+	}
+
+	return false
+
+}
