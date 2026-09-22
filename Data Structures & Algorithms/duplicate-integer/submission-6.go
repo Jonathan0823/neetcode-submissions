16ee29f30872ed5c10b56ebff2ad00733f1c3ecc
Guinn <146400304+Jonathan0823@users.noreply.github.com>
2026-09-22 11:06:16 +0700

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.go
@@ -0,0 +1,12 @@
+func hasDuplicate(nums []int) bool {
+    seen := make(map[int]struct{})
+    for _, num := range nums { 
+        if _, exist := seen[num]; exist { 
+            return true
+        }
+        seen[num] = struct{}{}
+    }
+
+    return false
+    
+}
